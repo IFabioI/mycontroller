6b4b7642cc7d27a6cb25924dc5e196a6c4a4d257
IFabioI <37349491+IFabioI@users.noreply.github.com>
2018-06-21 21:35:09 -0400

    Update Actor.java

--- modules/core/src/main/java/org/mycontroller/standalone/actor/Actor.java
+++ modules/core/src/main/java/org/mycontroller/standalone/actor/Actor.java
@@ -13,32 +13,52 @@ public class Actor {
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 		
+		Date currenttime = new Date();
+		
 		String usrInput;
 		
 		do {
 			usrInput = sc.next();
 			
-			switch(usrInput) {
+			String[] inputTokens = usrInput.split(" ");
+			
+			switch(inputTokens[0]) {
 				case "createCalendar":
 					baseCalendar.request(usrInput);
+					String response = Listen();
+					String id = response.split(" ")[0];
+					String name = response.split(" ")[1];
+					System.out.println("new calendar created, id: " + id = ", name: " + name);
 					break;
 				case "removeCalendar":
-					String name = usrInput.split(" ")[1];
+					String name = inputTokens[1];
 					if(areYouSure(name))
 						//name
 						baseCalendar.request(usrInput);
+					System.out.println("calendar " + name + " removed");
 					break;
 				case "editCalendarName":
 					//old name and new name
+					
 					baseCalendar.request(usrInput);
+					System.out.println("calendar " + inputTokens[1] + " changed to " + inputTokens[2]);
 					break;
 				case "openCalendar":
 					//name
 					baseCalendar.request(usrInput);
+					System.out.println("calendar " + inputTokens[1] + " opened");
 					break;
 				case "openCalendarDay":
 					//day
-					baseCalendar.request(usrInput);
+					baseCalendar.request("" + (currenttime.getYear() + 1900) + current.time.getMonth + inputTokens[1]);
+					int activationSize = Integer.parseInt(Listen());
+					for(int i = 0; i < activationSize; i++)
+					{
+						String response = Listen();
+						String sensorname = response.split(" ")[0];
+						Date time = new Date(Long.parseLong(response.split(" ")[1]));
+						System.out.println("sensor " + sensorname + " activated on " + time.getDate() + "/" + time.getMonth() + "/" + (time.getYear() + 1900) + " " + time.getHours() + ":" + time.getMinutes + ":" + time.getSeconds + " id: " + i);
+					}
 					break;
 				case "help":
 					showOptions();
@@ -66,8 +86,8 @@ public class Actor {
 		System.out.println("editCalendarName [old name] [new name] - replaces a "
 				+ "calendar's old name with a new name");
 		System.out.println("openCalendar [name] - opens calendar of name [name]");
-		System.out.println("openCalendarDay [name] [day] - opens visualization of "
-				+ "a specific day [day] on a specific calendar of name [name]");
+		System.out.println("openCalendarDay [day] - opens visualization of "
+				+ "a specific day [day]");
 		System.out.println("exit - exit system");
 	}
 	
